9eab9c04c91d651ff6da981840faddafb039640f
sakshikarule <sakshikarule11@gmail.com>
2025-10-12 23:39:34 +0530

    Add files via upload

---
+++ que4.c
@@ -0,0 +1,21 @@
+// write a program to print following patter
+/*
+ **
+ ***
+ ****
+ *****/*
+
+
+
+#include<stdio.h>
+int main()
+{
+    printf("*\n");
+    printf("**\n");
+    printf("***\n");
+    printf("****\n");
+    printf("*****\n");
+
+
+    return 0;
+}
