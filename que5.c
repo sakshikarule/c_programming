42e105bfd488c8c1404f8835ed2889f162ea64b9
sakshikarule <sakshikarule11@gmail.com>
2025-10-15 12:30:33 +0530

    Add files via upload

---
+++ que5.c
@@ -0,0 +1,16 @@
+// write a program to display ASCII value for charcter constant
+
+
+#include<stdio.h>
+int main()
+{
+   printf("The ascii vaule of character constant=%d\n", '\n');
+   printf("The ascii vaule of character constant=%d\n", '\t');
+   printf("the ascii vaule of character constant=%d\n", '\r');
+  // printf("the ascii vaule of character constant=%d\a", '\a');
+   //printf("the ascii vaule of character constant=%d\b", '\b');
+
+   
+    return 0;
+
+}
