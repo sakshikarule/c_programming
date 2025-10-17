a71fc2b295afffea2bac25f6f9e4afee409b2036
sakshikarule <sakshikarule11@gmail.com>
2025-10-17 12:28:29 +0530

    Add files via upload

---
+++ que6.c
@@ -0,0 +1,23 @@
+// write a program to accept an integer value and print its table.
+
+
+
+
+#include<stdio.h>
+int main()
+{
+  int num;
+  printf("enter any integer :");
+  scanf("%d",&num);
+
+  printf("%d * %d =%d\n",num,1,num*1);
+  printf("%d * %d =%d\n",num,2,num*3);
+  printf("%d * %d =%d\n",num,3,num*3);
+  printf("%d * %d =%d\n",num,4,num*4);
+  printf("%d * %d =%d\n",num,5,num*6);
+  printf("%d * %d =%d\n",num,6,num*6);
+  printf("%d * %d =%d\n",num,7,num*7);
+  printf("%d * %d =%d\n",num,8,num*8);
+  printf("%d * %d =%d\n",num,9,num*9);
+
+}
