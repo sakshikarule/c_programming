0c585c179daf9b4894ad315c7f84d5f61c64920a
sakshikarule <sakshikarule11@gmail.com>
2025-11-25 22:53:36 +0530

    Add files via upload

---
+++ Que2.c
@@ -0,0 +1,21 @@
+//odd or even
+
+
+#include<stdio.h>
+int main()
+{
+    int num1;
+
+    printf("enter any two number num1");
+    scanf("%d",&num1);
+  
+ if (num1 % 2==0)
+ {
+     printf("the number is Even");
+ }
+   else 
+   {
+    printf("the number is odd");
+   }
+
+}
